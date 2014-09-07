77bacca0afe6f01ff2287e1711bafc159e5ee4b4
goldenchan <wind.golden@gmail.com>
2014-09-07 10:49:24 +0800

    cleanup

--- public/index.php
+++ public/index.php
@@ -6,11 +6,6 @@
  //
  // Author: Chenjin (wind.golden@gmail.com)
  */
-if(isset($_GET['debug']))
-{
-    xhprof_enable(XHPROF_FLAGS_CPU+XHPROF_FLAGS_MEMORY);
-    $xhprof = true;
-}
 /**
  * 目录分割符
  */
@@ -26,25 +21,13 @@ define('SYS_ROOT_PATH', dirname(__DIR__) . DS . 'system' . DS);
 /**
  * 加载应用配置
  */
-require dirname(__DIR__) . DS . 'configs/app_config.php';
+require dirname(__DIR__) . DS . 'configs'.DS.'app_config.php';
 /**
  * bootstrap
  */
-require APP_ROOT_PATH . DS . "bootstrap.php";
+require APP_ROOT_PATH . "bootstrap.php";
 $router = new Router;
 $router->route(WI_CONFIG::$routes)->default_route(array(
     'default/Default',
     'error404'
 ))->execute();
-if(isset($xhprof))
-{
-    $xhprof_data = xhprof_disable();  
-    $XHPROF_ROOT = '/Users/golden/Sites/';
-    include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
-    include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
-
-    $xhprof_runs = new XHProfRuns_Default();
-    $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
-
-    echo "<a target=_blank href=http://localhost/html/xhprof_html/index.php?run=$run_id&source=xhprof_foo>xhProf</a>";
-}
